d5294d0a61e56c25261bd2daa7b66aa1a5fd4dda
Jessica Jones <daddehjones@gmail.com>
2021-06-03 12:32:27 -0400

    add ship test

--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -8,6 +8,17 @@ RSpec.describe Flotilla do
 
     expect(seventh_flotilla).to be_an_instance_of(Flotilla)
     expect(seventh_flotilla.name).to eq('Seventh Flotilla')
+    expect(seventh_flotilla.personnel).to eq([])
+    expect(seventh_flotilla.ships).to eq([])
+  end
+
+  it "adds ships" do
+    seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    seventh_flotilla.add_ship(daedalus)
+
+    expect(seventh_flotilla.ships).to eq([daedalus])
 
   end
 end
