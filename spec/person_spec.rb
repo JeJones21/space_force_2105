ba67819ad869b49b11be1dacf9f7d4e5c2dea943
Jessica Jones <daddehjones@gmail.com>
2021-06-03 12:55:11 -0400

    add recommend personnel test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -4,6 +4,9 @@ require './lib/person'
 RSpec.describe Person do
   it "exists and has attributes" do
     kathy = Person.new('Kathy Chan', 10)
+    polly = Person.new('Polly Parker', 8)
+    rover = Person.new('Rover Henriette', 1)
+    sampson = Person.new('Sampson Edwards', 7)
 
     expect(kathy).to be_an_instance_of(Person)
     expect(kathy.name).to eq("Kathy Chan")
@@ -13,10 +16,20 @@ RSpec.describe Person do
 
   it "starts without specialties and adds them" do
       kathy = Person.new('Kathy Chan', 10)
+      polly = Person.new('Polly Parker', 8)
+      rover = Person.new('Rover Henriette', 1)
+      sampson = Person.new('Sampson Edwards', 7)
 
       kathy.add_specialty(:astrophysics)
       kathy.add_specialty(:quantum_mechanics)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
 
       expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+
   end
 end
