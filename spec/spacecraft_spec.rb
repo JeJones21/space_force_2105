cb785f114a125de39598206d6f9fc844b8f24a2f
Jessica Jones <daddehjones@gmail.com>
2021-06-03 11:45:16 -0400

    add requirement test

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Spacecraft do
     expect(daedalus.fuel).to eq(400)
     expect(daedalus.requirements).to eq([])
   end
+
+  it "adds requirements" do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    daedalus.add_requirement({astrophysics: 6})
+    daedalus.add_requirement({quantum_mechanics: 3})
+
+    expect(daedalus.requirements).to eq([{astrophysics: 6}, {quantum_mechanics: 3}])
+
+  end
 end
