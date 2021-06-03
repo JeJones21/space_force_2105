f9e0c46fbd46158ad7205013187a52244b8053f4
Jessica Jones <daddehjones@gmail.com>
2021-06-03 11:25:20 -0400

    create specialty test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Person do
     expect(kathy.experience).to eq(10)
     expect(kathy.specialties).to eq([])
   end
+
+  it "starts without specialties and adds them" do
+      kathy = Person.new('Kathy Chan', 10)
+
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+  end
 end
