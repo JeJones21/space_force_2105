c8fb86844a0aa4945ea1aef01f3d5eab5dd978e7
Jessica Jones <daddehjones@gmail.com>
2021-06-03 11:08:37 -0400

    initial commit

---
+++ spec/spacecraft_spec.rb
@@ -0,0 +1,12 @@
+require './lib/spacecraft'
+require './lib/person'
+
+RSpec.describe Spacecraft do
+  it "exists and has attributes" do
+    daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+    expect(daedalus).to be_an_instance_of(Spacecraft)
+    expect(daedalus.name).to eq('Daedalus')
+    expect(daedalus.fuel).to eq(400)
+  end
+end
