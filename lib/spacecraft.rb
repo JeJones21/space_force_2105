ce42712e69324617d0fb2c71cf6ec216a40921f7
Jessica Jones <daddehjones@gmail.com>
2021-06-03 11:41:28 -0400

    add requirements

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,9 +1,12 @@
 class Spacecraft
   attr_reader :name,
-              :fuel
+              :fuel,
+              :requirements
 
   def initialize(info)
     @name = info[:name]
     @fuel = info[:fuel]
+    @requirements = []
   end
+
 end
